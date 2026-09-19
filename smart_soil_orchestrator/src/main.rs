2befc5928509486560699c296d63a0d37217fc15
ananthalokantara6-art <ananthalokantara6@gmail.com>
2026-09-19 10:23:36 +0800

    feat: update main.rs with pipeline functions

--- smart_soil_orchestrator/src/main.rs
+++ smart_soil_orchestrator/src/main.rs
@@ -1,10 +1,10 @@
-//mengirim data ke modul 2 
+///mengirim data ke modul 2 
 pub fn send_data(...) {
 //isi codingannya     
 
 }
 
-//menerima data dari modul 1
+///menerima data dari modul 1
 pub fn get_data(...) {
 //isi codingannya
 }
@@ -14,13 +14,35 @@ pub fn receive_sensor_data(...) {
 //isi codingannya
 }
 
-pub fn getcurrentsoildata(...) 
-//isi codingannya
-
-pub fn gethistoralsoildata(...) {
+///mengambil data dari modul 3
+pub fn get_current_soil_data(...) {
+    //isi codingannya
 
 }
 
-pub fn m4_to_m5(...) {
+pub fn get_historical_soil_data(...) {
+
+}
+
+///mengambil data dari modul 4
+pub fn get_soil_assessment_data(...) {
 //isi codingannya
+}
+
+///module 5
+pub fn get_plan_rules(...) {
+    //isi codingannya
+}
+
+///moudle 6
+pub fn register_plugin(...) {
+    //isi codingannya
+}
+
+pub fn discover_plugins(...) {
+    //isi codingannya
+}
+
+pub fn execute_plugin(...) {
+    //isi codingannya
 }
