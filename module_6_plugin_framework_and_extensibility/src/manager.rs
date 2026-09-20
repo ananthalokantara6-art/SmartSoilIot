cb5254728e208c39d211c6a49991644967dc2478
alcanary <ahmadqowi03@gmail.com>
2026-09-20 21:24:44 +0800

    Definisi fungsi Validation & Execution

--- module_6_plugin_framework_and_extensibility/src/manager.rs
+++ module_6_plugin_framework_and_extensibility/src/manager.rs
@@ -1,10 +1,12 @@
 use crate::plugin::Plugin;
 use crate::metadata::PluginMetadata;
 use crate::error::PluginError;
+use crate::plugin_io::*;
 
 pub struct PluginManager {}
 
 impl PluginManager {
+    // Registration & Discovery
     pub fn register(
         &mut self,
         plugin: Box<dyn Plugin>,
@@ -43,4 +45,67 @@ impl PluginManager {
     ) -> Result<Vec<PluginMetadata>, PluginError>{
         todo!()
     }
+
+    // Validation & Lifecycle
+
+    pub fn validate(
+        &self,
+        plugin: &dyn Plugin,
+    ) -> Result<(), PluginError> {
+        todo!()
+    }
+
+    pub fn enable(
+        &mut self,
+        plugin_id: &str,
+    ) -> Result<(), PluginError> {
+        todo!()
+    }
+
+    pub fn disable(
+        &mut self,
+        plugin_id: &str,
+    ) -> Result<(), PluginError> {
+        todo!()
+    }
+
+    pub fn initialize(
+        &mut self,
+        plugin_id: &str,
+    ) -> Result<(), PluginError> {
+        todo!()
+    }
+
+    pub fn shutdown(
+        &mut self,
+        plugin_id: &str,
+    ) -> Result<(), PluginError> {
+        todo!()
+    } 
+
+    // Plugin Execution
+
+    pub fn execute_analytics(
+        &self,
+        plugin_id: &str,
+        input: AnalyticsInput,
+    ) -> Result<AnalyticsOutput, PluginError> {
+        todo!()
+    }
+
+    pub fn execute_model(
+        &self,
+        plugin_id: &str,
+        input: ModelInput,
+    ) -> Result<ModelOutput, PluginError> {
+        todo!()
+    }
+
+    pub fn execute_plant_rule(
+        &self,
+        plugin_id: &str,
+        input: PlantRuleInput,
+    ) -> Result<PlantRuleOutput, PluginError> {
+        todo!()
+    } 
 }
