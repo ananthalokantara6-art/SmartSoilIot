9f6f4d04d7fc0218303d59139f1d4615241123c7
alcanary <ahmadqowi03@gmail.com>
2026-09-21 00:42:12 +0800

    Rustdoc for plugin

--- module_6_plugin_framework_and_extensibility/src/plugin.rs
+++ module_6_plugin_framework_and_extensibility/src/plugin.rs
@@ -1,27 +1,58 @@
 use crate::metadata::PluginMetadata;
 use crate::error::PluginError;
 use crate::plugin_io::*;
-
+/// Basis interface yang digunakan seluruh plugin.
 pub trait Plugin {
-
+    /// Mengembalikan referensi metadata dari plugin.
     fn metadata(&self)-> &PluginMetadata;
 }
 
+/// Interface plugin analitik yang berhubungan dengan Module-4.
 pub trait AnalyticsPlugin: Plugin {
+    /// Analisa data dari sensor dan statistik.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `input` - Soil data yang akan diproses olehh plugin.
+    /// 
+    /// # Returns
+    /// Kembalikan data analisis, atau "PluginError" apabila
+    /// analisis tidak dapat diselesaikan.
     fn analyze(
         &self,
         input: AnalyticsInput,
     ) -> Result<AnalyticsOutput, PluginError>;
 }
 
+/// Interface plugin pemrosesan data menggunakan AI berhubungan dengan Module-4.
 pub trait ModelPlugin: Plugin {
+    /// Proses data sensor dan statistik menggunakan model AI.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `input` - Soil data yang akan diproses olehh plugin.
+    /// 
+    /// # Returns
+    /// Kembalikan output AI, atau "PluginError" apabila
+    /// proses tidak dapat diselesaikan.
     fn predict(
         &self,
         input: ModelInput,
     ) -> Result<ModelOutput, PluginError>;
 }
 
+/// Interface plugin untuk pemrosesan data pada jenis tanaman tertentu
 pub trait PlantRulePlugin: Plugin {
+    /// Proses data sensor dan statistik berdasarkan dan rekomendasi
+    /// untuk jenis tanaman tertentu.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `input` - Soil data yang akan diproses olehh plugin.
+    /// 
+    /// # Returns
+    /// Kembalikan aturan spesifik tumbuhan, atau "PluginError" apabila
+    /// proses tidak dapat diselesaikan.
     fn evaluate(
         &self,
         input: PlantRuleInput,
